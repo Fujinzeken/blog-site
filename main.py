0589e94d5beeb854637bbe81b76a4a7d63081f9d
micheal <fujinzeken@yahoo.com>
2022-04-08 13:18:20 +0100

    change sqlite to postgres

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import re
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = uri #os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
